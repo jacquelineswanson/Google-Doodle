faa55934c090d4728a0b106cc63430e9b3a8e857
jacquelineswanson <swanson.jacque@gmail.com>
2020-02-23 23:58:35 -0800

    Stars
    Added star elements to HTML, CSS, and Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log('OK');
+
+gsap.from(".star", {
+    opacity: 0,  
+    duration: 2.5,
+    delay: 1,
+  });
+  
+  gsap.from(".smstar", {
+    opacity: 0,  
+    duration: 2.5,
+    delay: 1.15,
+  });
+  
+  gsap.to("#s30", {
+    x: 400,
+    y: 100,
+    delay: 3,
+    duration: 1.5,
+    
+  });
